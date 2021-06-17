b5c25e27254215e481b224e8b245c3601c040840
Shiro <shiro.archerl@gmail.com>
2021-06-17 11:15:12 -0500

    Create Tarea2.js

---
+++ Adri/Tarea2.js
@@ -0,0 +1,60 @@
+var mes = prompt("en que mes naciste?");
+
+//if (mes === "str")
+//{
+//mes.toLowerCase()
+//}
+//else 
+//{}
+if ((mes == "enero")||(mes == "ENERO")||(mes == 1))
+{
+console.log("Eres Capricornio");
+}
+else if ((mes == "febrero")||(mes == "FEBRERO")||(mes == 2))
+{
+console.log("Eres Acuario");
+}
+else if ((mes == "marzo")||(mes == "MARZO")||(mes == 3))
+{
+console.log("Eres Picis");
+}
+else if ((mes == "abril")||(mes == "ABRIL")||(mes == 4))
+{
+console.log("Eres Aries");
+}
+else if ((mes == "mayo")||(mes == "MAYO")||(mes == 5))
+{
+console.log("Eres Tauro");
+}
+else if ((mes == "junio")||(mes == "JUNIO")||(mes == 6))
+{
+console.log("Eres Geminis");
+}
+else if ((mes == "julio")||(mes == "JULIO")||(mes == 7))
+{
+console.log("Eres Cancer");
+}
+else if ((mes == "agosto")||(mes == "AGOSTO")||(mes == 8))
+{
+console.log("Eres Leo");
+}
+else if ((mes == "septiembre")||(mes == "SEPTIEMBRE")||(mes == 9))
+{
+console.log("Eres Virgo");
+}
+else if ((mes == "octubre")||(mes == "OCTUBRE")||(mes == 10))
+{
+console.log("Eres Libra");
+}
+else if ((mes == "noviembre")||(mes == "NOVIEMBRE")||(mes == 11))
+{
+console.log("Eres Escorpio");
+}
+else if ((mes == "diciembre")||(mes == "DICIEMBRE")||(mes == 12))
+{
+console.log("Eres Sagitario");
+}
+else 
+{
+console.log("chtm eso no es un mes");
+}
