3ad7e4046a94467d42804a40ec9e3771e673e0fa
Shiro <shiro.archerl@gmail.com>
2021-06-16 23:07:21 -0500

    Create Tarea2.js

---
+++ Mau/Tarea2.js
@@ -0,0 +1,49 @@
+var mes = prompt("Escribe bien tu mes de nacimiento o en su defecto el número del mes:")
+if ((mes == 1) || (mes == "Enero"))
+{
+console.log("Capricornio")
+}
+else if ((mes == 2) || (mes == "Febrero"))
+{
+console.log("Acuario")
+}
+else if ((mes == 3) || (mes == "Marzo"))
+{
+console.log("Piscis")
+}
+else if ((mes == 4) || (mes == "Abril"))
+{
+console.log("Aries")
+}
+else if ((mes == 5) || (mes == "Mayo"))
+{
+console.log("Tauro")
+}
+else if ((mes == 6) || (mes == "Junio"))
+{
+console.log("Géminis")
+}
+else if ((mes == 7) || (mes == "Julio"))
+{
+console.log("Cáncer")
+}
+else if ((mes == 8) || (mes == "Agosto"))
+{
+console.log("Leo")
+}
+else if ((mes == 9) || (mes == "Septiembre"))
+{
+console.log("Virgo")
+}
+else if ((mes == 10) || (mes == "Octubre"))
+{
+console.log("Libra")
+}
+else if ((mes == 11) || (mes == "Noviembre"))
+{
+console.log("Escorpio")
+}
+else 
+{
+console.log("Sagitario")
+}
