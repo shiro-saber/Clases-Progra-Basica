deba8352a0bf7e46ea6c8ca57cefc4456c223445
Shiro <shiro.archerl@gmail.com>
2021-06-16 23:06:44 -0500

    Create Tarea2.js

---
+++ MaFer/Tarea2.js
@@ -0,0 +1,54 @@
+var mes = prompt("Please enter a month.")
+
+if ((mes == 1) || (mes == "Enero") || mes ==("enero")) 
+{
+  console.log("Capricornio")
+}
+else if ((mes == 2) || (mes == "Febrero") || mes ==("febrero")) 
+{
+  console.log("Acuario")
+}
+else if ((mes == 3) || (mes == "Marzo") || mes ==("marzo")) 
+{
+  console.log("Picis")
+}
+else if ((mes == 4) || (mes == "Abril") || mes ==("abril")) 
+{
+  console.log("Aries")
+}
+else if ((mes == 5) || (mes == "Mayo") || mes ==("mayo")) 
+{
+  console.log("Tauro")
+}
+else if ((mes == 6) || (mes == "Junio") || mes ==("junio")) 
+{
+  console.log("Geminis")
+}
+else if ((mes == 7) || (mes == "Julio") || mes ==("julio")) 
+{
+  console.log("Cancer")
+}
+else if ((mes == 8) || (mes == "Agosto") || mes ==("agosto")) 
+{
+  console.log("Leo")
+}
+else if ((mes == 9) || (mes == "Septiembre") || mes ==("septiembre")) 
+{
+  console.log("Virgo")
+}
+else if ((mes == 10) || (mes == "Octubre") || mes ==("octubre")) 
+{
+  console.log("Libra")
+}
+else if ((mes == 11) || (mes == "Noviembre") || mes ==("noviembre")) 
+{
+  console.log("Escorpio")
+}
+else if ((mes == 12) || (mes == "diciembre") || mes ==("diciembre")) 
+{
+  console.log("Sagitario")
+}
+else 
+{
+  console.log("ʕ•́ᴥ•̀ʔっ That's not a month BIATCH !!!")
+}
