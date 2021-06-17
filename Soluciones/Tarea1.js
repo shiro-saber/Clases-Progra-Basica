49c458e5b0dc7e5caa5f9ff9a357517aec546f99
Shiro <shiro.archerl@gmail.com>
2021-06-16 23:03:27 -0500

    Create Tarea1.js

---
+++ Soluciones/Tarea1.js
@@ -0,0 +1,10 @@
+/** 
+ * Recibir del usuario un número e
+ * imprimir si dicho número es par o impar
+ */
+
+// Pide el número al usuario.
+var numero = prompt("Dame un número");
+
+// inline if
+numero % 2 == 0 ? console.log("Es número par") : console.log("Es impar");
