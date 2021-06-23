c60627209315883803f89b6d0f64f151715fd219
Shiro <shiro.archerl@gmail.com>
2021-06-23 14:31:04 -0500

    Create Tarea1.js

---
+++ Max/Tarea1.js
@@ -0,0 +1,13 @@
+//Promt the user for a number
+const number = prompt("Please enter a number: ");
+
+//check if number is even
+if (number % 2 == 0)
+{
+	console.log("Yahooo! The number is even");
+}
+//if the number is odd
+else 
+{
+	console.log("Yep, you have an odd number mate.");
+}
