7127a102efe5f6637189a9a9c7b0004a16492dfa
Shiro <shiro.archerl@gmail.com>
2021-09-13 11:41:07 -0500

    Create Tarea3.js

---
+++ MaFer/Tarea3.js
@@ -0,0 +1,67 @@
+var day = prompt(` Ingresa tu día de nacimiento`);
+var month = prompt(` Ingresa tu mes de nacimiento en minusculas`)
+
+if (((day >= 20) && (month == "enero")) || ((day <= 19) && (month == "febrero"))) 
+{
+	console.log("Acuario: Se avecinan cambios importantes con grandes beneficios en donde no estás incluido.")
+}
+
+else if (((day >= 20) && (month == "febrero")) || ((day <= 19) && (month == "marzo"))) 
+{
+	console.log("Picis: Hoy la vas a cagar bien y bonito, los astros te recomiendan abrir la boca sólo pa tragar.")
+}
+
+else if (((day >= 20) && (month == "marzo")) || ((day <= 19) && (month == "abril"))) 
+{
+	console.log("Aries: Analiza las cosas desde una perspectiva inteligente y no tu usual pendeja idiota mentalidad, plis.")
+}
+
+else if (((day >= 20) && (month == "abril")) || ((day <= 19) && (month == "mayo"))) 
+{
+	console.log("Tauro: Hoy rodéate de gente menos pendeja o al menos desapendéjate un poco, hazlo por tu bien")
+}
+
+else if (((day >= 20) && (month == "mayo")) || ((day <= 19) && (month == "junio"))) 
+{
+	console.log("Geminis: Eres mamone por naturaleza y eres idiota por convicción por lo que recomiendo, te alejes de aquellas personas a las que te dolería mandar a la verga.")
+}
+
+else if (((day >= 20) && (month == "junio")) || ((day <= 19) && (month == "julio"))) 
+{
+	console.log("Cancer: Esta semana habrá pedo, debes andar al pedo o vas a valer pa pura madre.")
+}
+
+else if (((day >= 20) && (month == "julio")) || ((day <= 19) && (month == "agosto"))) 
+{
+	console.log("Leo: Hoy puedes tener un día maravilloso pero la gente cercana a ti ya te conoce y sabe que en algún momento la vas a cagar... y recio.")
+}
+
+else if (((day >= 20) && (month == "agosto")) || ((day <= 19) && (month == "septiembre"))) 
+{
+	console.log("Virgo: Estas hasta la madre y bien puteade, recuéstate, al fin y al cabo nunca haces nada, pinche inútil.")
+}
+
+else if (((day >= 20) && (month == "septiembre")) || ((day <= 19) && (month == "octubre"))) 
+{
+	console.log("Libra: Tus acciones caen bajo la influencia de Marte por lo que querrásponerte de verguero a cada pinche rato, desquítate con quien puedas y que chingue su madre el América.")
+}
+
+else if (((day >= 20) && (month == "octubre")) || ((day <= 19) && (month == "noviembre"))) 
+{
+	console.log("Escorpio: Tu planeta regente se encuentra tratando de arreglar tus mamadas pero ni el mismísimo Júpiter puede con tanta mierda, no mames.")
+}
+
+else if (((day >= 20) && (month == "noviembre")) || ((day <= 19) && (month == "diciembre"))) 
+{
+	console.log("Sagitario: Hoy será tu día de quejitis aguda por lo que le caerás a todos en la punta de los huevos, sólo trata de no ser une hije de puta")
+}
+
+else if (((day >= 20) && (month == "diciembre")) || ((day <= 19) && (month == "enero"))) 
+{
+	console.log("Capricornio: Putón que ya no es planeta en conjunción con Tuano nos dicen que es día de echar la hueva a más no poder, total, ya has de estar acostumbrade.")
+}
+else {
+console.log ("No importa el signo, lo importante es saber hacia dónde enfocar nuestras energías, de todos modos, nomás la cagamos.")
+}
+
+console.log("P.D. A JC le gusta por atrás")
